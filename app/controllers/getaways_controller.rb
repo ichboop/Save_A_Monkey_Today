7b29511c945fc3fee1db4d02d4231e8711bd551a
Daniel Thiel <dthiel.berlin@gmail.com>
2021-02-23 17:22:54 +0100

    added user authentication

--- app/controllers/getaways_controller.rb
+++ app/controllers/getaways_controller.rb
@@ -1,5 +1,5 @@
 class GetawaysController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index]
   def show
     @monkey = Monkey.find(28)
   end
