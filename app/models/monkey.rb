923b9eae73525c69e808821efc4140a75cf5b472
Leonard Diemer <leonard.ow.diemer@gmail.com>
2021-02-24 15:09:27 +0100

    t2

--- app/models/monkey.rb
+++ app/models/monkey.rb
@@ -4,5 +4,6 @@ class Monkey < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :city, :species, presence: true
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # after_validation :geocode
+  # , if: :will_save_change_to_address?
 end
