d6a42f76d749160eb7d9953e5f20f357c3779487
Daniel Thiel <dthiel.berlin@gmail.com>
2021-02-24 11:41:50 +0100

    new seed + show getaways of a specific user

--- app/controllers/getaways_controller.rb
+++ app/controllers/getaways_controller.rb
@@ -1,10 +1,13 @@
 class GetawaysController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def show
-    @getaways = Getaway.where(user_id: 35)
-    @monkeys = @getaways.map do |getaway|
-      Monkey.where(id: getaway.monkey_id)
+    @getaways = Getaway.where(user_id: 435)
+    @monkeys = []
+    @getaways.each do |getaway|
+      @monkeys << Monkey.find(getaway.monkey_id)
     end
+    
+
   end
 
   def new
