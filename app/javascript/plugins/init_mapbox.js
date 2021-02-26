d4d87b54a7ce41cbb22e511d6cac39b3c4dfc269
Leonard Diemer <leonard.ow.diemer@gmail.com>
2021-02-26 14:39:39 +0100

    map geocoder search

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -27,10 +27,9 @@ const addMarkersToMap = (map, markers) => {
 };
 
 const fitMapToMarkers = (map, markers) => {
-  console.log(markers);
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 3000});
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0});
 };
 
 const initMapbox = () => {
